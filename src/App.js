faca469b52bdf49f0b34097dbb996e4c3635bd64
Bhoomi1503 <bhoomiya1503@gmail.com>
2024-08-19 09:48:51 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-
 import './App.css';
 import Header from '../src/component/header.js';
-
 function App() {
   return (
     <div className="App">
@@ -9,5 +7,4 @@ function App() {
     </div>
   );
 }
-
 export default App;
